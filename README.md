cf3d69b624a7b9d1ff74326178edfc7d07e99f36
krande <kristoffer_andersen@outlook.com>
2021-04-16 21:14:50 +0200

    Minor fixes to docs. Small edit to io meshio writer. Added link to Salome Meca v9.6.0 installation files for windows (and linux) to README.md.

--- README.md
+++ README.md
@@ -146,8 +146,9 @@ Settings.fem_exe_paths["abaqus"] = "<path to your abaqus.bat>"
 For installation files of open source FEM software such as Calculix and Code Aster, here are some links:
 
 * https://github.com/calculix/cae/releases (calculix CAE for windows/linux)
-* https://code-aster-windows.com/download/ (Code Aster for Windows)
+* https://code-aster-windows.com/download/ (Code Aster for Windows Salome Meca v9.3)
 * https://www.code-aster.org/spip.php?rubrique21 (Code Aster for Linux)
+* https://salome-platform.org/downloads/current-version (Salome Meca v9.6 for windows/linux)
 
 ## For developers
 
--- src/ada/fem/io/io_meshio/writer.py
+++ src/ada/fem/io/io_meshio/writer.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from itertools import groupby
 from operator import attrgetter
@@ -75,24 +76,23 @@ def fem_to_meshio(fem):
     """
     from . import ada_to_meshio_type
 
-    def get_nids(el):
-        return [n.id for n in el.nodes]
-
-    cells = []
-    plist = list(sorted(fem.nodes, key=attrgetter("id")))
-    if len(plist) == 0:
-        return None
-
-    pid = plist[-1].id
-    points = np.zeros((int(pid + 1), 3))
+    # Points
+    points = np.zeros((int(fem.nodes.max_nid + 1), 3))
 
     def pmap(n):
         points[n.id] = n.p
 
     list(map(pmap, fem.nodes))
+
+    # Elements
+    cells = []
+
+    def get_nids(el):
+        return [n.id for n in el.nodes]
+
     for group, elements in groupby(fem.elements, key=attrgetter("type")):
         if group in ElemShapes.masses + ElemShapes.springs:
-            # Do something
+            logging.error('NotImplemented: Skipping Mass or Spring Elements')
             continue
         med_el = ada_to_meshio_type[group]
         el_mapped = np.array(list(map(get_nids, elements)))
