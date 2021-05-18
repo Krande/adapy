9dbf4fbc3f9a05c2244559fb9d4a508248bb58a0
ofskrand <kristoffer_andersen@outlook.com>
2021-05-18 15:40:37 +0200

    Fix bug in get_list_of_files() function

--- src/ada/core/utils.py
+++ src/ada/core/utils.py
@@ -2045,7 +2045,7 @@ def get_list_of_files(dir_path, file_ext=None, strict=False):
         full_path = os.path.join(dir_path, entry)
         # If entry is a directory then get the list of files in this directory
         if os.path.isdir(full_path):
-            all_files = all_files + get_list_of_files(full_path)
+            all_files = all_files + get_list_of_files(full_path, file_ext, strict)
         else:
             all_files.append(full_path)
 
@@ -2057,7 +2057,7 @@ def get_list_of_files(dir_path, file_ext=None, strict=False):
         if strict:
             raise FileNotFoundError(msg)
         else:
-            warnings.warn(msg)
+            logging.info(msg)
 
     return all_files
 
