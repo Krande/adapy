98863f3139a7703a390c72f51ec6267db3e495dc
krande <kristoffer_andersen@outlook.com>
2021-06-22 13:26:46 +0200

    Updated name as the intention is to no longer just handles uniformly distributed loads

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,13 +1,13 @@
 import unittest
 
 from ada import Beam
-from ada.calc.udl import UDL
+from ada.calc import BeamCalc
 
 
 class TestCalculations(unittest.TestCase):
     def test_basic_udl(self):
         bm = Beam("MyBeam", (0, 0, 0), (1, 0, 0), "IPE300")
-        udl = UDL(bm)
+        udl = BeamCalc(bm)
         udl.add_distributed_load(-1e3)
         udl._repr_html_()
 
