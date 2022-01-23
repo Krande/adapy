48eaa196f2ac3d816fd4f664084f458f207300cf
krande <kristoffer_andersen@outlook.com>
2022-01-23 19:17:39 +0100

    Further work on IFC

--- src/ada/ifc/write/write_instances.py
+++ src/ada/ifc/write/write_instances.py
@@ -14,7 +14,7 @@ def write_mapped_instance(instance: "Instance", f: "file"):
     if len(products) != 1:
         raise ValueError(f'Unable to find IFC element with guid="{elem.guid}"')
     ifc_elem = products[0]
-    origin = create_ifc_placement(f)
+    origin = create_ifc_placement(f)  # , loc_z=elem.xvec.astype(float).tolist()
     body = ifc_elem.Representation.Representations[0]
 
     rep_map = f.create_entity("IFCREPRESENTATIONMAP", origin, body)
@@ -38,7 +38,9 @@ def write_mapped_instance(instance: "Instance", f: "file"):
         "IFCSHAPEREPRESENTATION", body.ContextOfItems, "body", "MappedRepresentation", mapped_instances
     )
 
-    prod_def_shape = f.create_entity("IFCPRODUCTDEFINITIONSHAPE", None, None, [shape_rep])
+    prod_def_shape = f.create_entity(
+        "IFCPRODUCTDEFINITIONSHAPE", Name=None, Description=None, Representations=[shape_rep]
+    )
 
     f.create_entity(
         "IFCBUILDINGELEMENTPROXY",
