02c142b561bdab0199cf319782159d782b150c32
krande <kristoffer_andersen@outlook.com>
2022-02-20 10:36:51 +0100

    fix: now working merging of polygons for custom json

--- src/ada/visualize/formats/custom_json/merge_utils.py
+++ src/ada/visualize/formats/custom_json/merge_utils.py
@@ -5,30 +5,34 @@ from typing import TYPE_CHECKING, Dict, Iterable, List, Union
 import numpy as np
 
 from ada.ifc.utils import create_guid
-from ada.visualize.utils import convert_obj_to_poly
+from ada.visualize.formats.custom_json.write_objects_to_json import obj_to_json
 
 if TYPE_CHECKING:
     from ada import Beam, Plate
 
 
-def merge_objects_into_single_json(guid, colour, opacity, list_of_objects: Iterable[Union[Beam, Plate]]) -> dict:
+def merge_objects_into_single_json(colour, opacity, list_of_objects: Iterable[Union[Beam, Plate]]) -> dict:
     id_sequence = dict()
     indices = np.array([], dtype=int)
     position = np.array([], dtype=float)
+    normals = np.array([], dtype=float)
+
     for obj in list_of_objects:
-        res = convert_obj_to_poly(obj)
-        pos_len = int(len(position) / 6)
+        res = obj_to_json(obj)
+        pos_len = int(len(position) / 3)
         new_index = np.array(res["index"]) + pos_len
         mi, ma = min(new_index), max(new_index)
-        position = np.concatenate([np.array(position), res["position"]])
+        position = np.concatenate([position, np.array(res["position"])])
+        normals = np.concatenate([position, np.array(res["normal"])])
         indices = np.concatenate([indices, new_index])
         id_sequence[obj.guid] = (int(mi), int(ma))
 
     return dict(
-        guid=guid,
         index=indices.astype(int).tolist(),
         position=position.flatten().astype(float).tolist(),
+        normal=normals.flatten().astype(float).tolist(),
         color=[*colour, opacity],
+        vertexColor=None,
         instances=[],
         id_sequence=id_sequence,
     )
@@ -47,7 +51,7 @@ def merge_by_colours(name, list_of_objects: Iterable[Union[Beam, Plate]]):
     for colour, elements in colour_map.items():
         el0 = elements[0]
         guid = create_guid()
-        id_map[guid] = merge_objects_into_single_json(el0.guid, el0.colour_norm, el0.opacity, elements)
+        id_map[guid] = merge_objects_into_single_json(el0.colour_norm, el0.opacity, elements)
 
     merged_part = {
         "name": name,
