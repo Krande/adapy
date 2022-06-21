95cde9af6bbeef0d512b71a050dcdca2cb560a7e
krande <kristoffer_andersen@outlook.com>
2022-06-21 15:12:54 +0200

    chore: further reduce IFC element duplication when writing IFC files

--- src/ada/ifc/concepts.py
+++ src/ada/ifc/concepts.py
@@ -7,6 +7,7 @@ from typing import TYPE_CHECKING, Union
 
 if TYPE_CHECKING:
     import ifcopenshell.geom
+    from ada import Assembly
 
 
 @dataclass
@@ -17,3 +18,18 @@ class IfcRef:
         import ifcopenshell.geom
 
         return ifcopenshell.geom.create_shape(settings, inst=ifc_elem)
+
+
+@dataclass
+class IfcIOBase:
+    f: ifcopenshell.file = None
+    a: Assembly = None
+
+
+@dataclass
+class IfcIO(IfcIOBase):
+    def from_ifc(self, ifc_file: str | pathlib.Path) -> Assembly:
+        ...
+
+    def to_ifc(self, assembly: Assembly, ifc_file: str | pathlib.Path):
+        ...
