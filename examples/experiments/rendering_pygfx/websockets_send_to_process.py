03f45fe9fbc8e575f8aa2eea28372e8da9f5f3c3
krande <kristoffer_andersen@outlook.com>
2023-06-28 21:38:14 +0200

    minor change

--- examples/experiments/rendering_pygfx/websockets_send_to_process.py
+++ examples/experiments/rendering_pygfx/websockets_send_to_process.py
@@ -6,8 +6,8 @@ from websockets.sync.client import connect
 import ada
 
 
-def hello():
-    bm = ada.Beam("my_beam_x", (2, 0, 0), (2, 0, 1), "IPE300", color="red")
+def send_to_viewer():
+    bm = ada.Beam("my_beam_x", (1, 0, 0), (1, 0, 1), "IPE300", color="red")
     a = ada.Assembly() / bm
     data = io.BytesIO()
     a.to_trimesh_scene().export(data, file_type="glb")
@@ -16,4 +16,4 @@ def hello():
 
 
 if __name__ == "__main__":
-    hello()
+    send_to_viewer()
