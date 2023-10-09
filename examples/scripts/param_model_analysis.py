a63ed0119bc94785607458166cb6c3899386adcd
krande <kristoffer_andersen@outlook.com>
2023-10-09 17:34:56 +0200

    debug issues with created med files

--- examples/scripts/param_model_analysis.py
+++ examples/scripts/param_model_analysis.py
@@ -17,14 +17,18 @@ def eigen_step():
 
 
 def main():
+    fem_res = "ca_param_model_ca"
+    res_file = (SCRATCH / fem_res / fem_res).with_suffix(".rmed")
+
     p = SimpleStru("MyStru")
-    p.fem = p.to_fem_obj(0.1, use_quads=True)
+    p.fem = p.to_fem_obj(0.1, use_quads=False)
     p.add_bcs()
     a = ada.Assembly("ParametricSite") / p
     # a.fem.add_step(gravity_step())
     a.fem.add_step(eigen_step())
-    res = a.to_fem("ca_param_model_ca", "code_aster", overwrite=True, execute=True, scratch_dir=SCRATCH)
-    res.to_xdmf(res.name + ".xdmf")
+    res = a.to_fem(fem_res, "code_aster", overwrite=True, execute=True, scratch_dir=SCRATCH)
+    mesh = res.to_meshio_mesh(make_3xn_dofs=True)
+    mesh.write(res_file.with_suffix('.vtu'))
     # res.to_vtu()
 
     # a.to_fem("ca_param_model_ses", "sesam", overwrite=True, execute=True)
@@ -40,5 +44,5 @@ def read_res():
 
 
 if __name__ == "__main__":
-    # main()
-    read_res()
+    main()
+    # read_res()
