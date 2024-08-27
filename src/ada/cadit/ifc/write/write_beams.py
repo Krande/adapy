7c210a049959ff867600a90a784b93731195ed6e
krande <kristoffer_andersen@outlook.com>
2024-08-27 13:52:00 +0200

    bring back relmat

--- src/ada/cadit/ifc/write/write_beams.py
+++ src/ada/cadit/ifc/write/write_beams.py
@@ -110,9 +110,9 @@ class IfcBeamWriter:
         )
         mat_profile_set = f.createIfcMaterialProfileSet(sec.name, None, [mat_profile], None)
 
-        _ = f.create_entity("IfcMaterialProfileSetUsage", mat_profile_set, convert_bm_jusl_to_ifc(beam))
+        mat_usage = f.create_entity("IfcMaterialProfileSetUsage", mat_profile_set, convert_bm_jusl_to_ifc(beam))
 
-        # ifc_store.writer.create_rel_associates_material(create_guid(), mat_usage, [ifc_beam])
+        ifc_store.writer.create_rel_associates_material(create_guid(), mat_usage, [ifc_beam])
         ifc_store.writer.associate_elem_with_material(beam.material, ifc_beam)
 
         return mat_profile_set
