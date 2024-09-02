5843d44cc0f1d95b3e7ecd70f723e7a9dde561ea
krande <kristoffer_andersen@outlook.com>
2024-09-02 16:43:09 +0200

    remove benchmarking outside of profile dir

--- tests/profiling/test_large_ifc_models.py
+++ tests/profiling/test_large_ifc_models.py
@@ -2,12 +2,12 @@ import pytest
 
 import ada
 
-
+@pytest.mark.parametrize("num_beams", [100, 1_000, 5_000, 10_000])
 @pytest.mark.benchmark
-def test_build_big_ifc():
+def test_build_big_ifc(num_beams):
     # create beams and write it to ifc
     beams = []
-    for i in range(0, 5_000):
+    for i in range(0, num_beams):
         bm = ada.Beam(f"bm{i}", (i, 0, 0), (i + 1, 0, 0), "IPE300")
         beams.append(bm)
     a = ada.Assembly() / beams
