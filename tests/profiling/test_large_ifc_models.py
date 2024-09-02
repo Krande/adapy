7e6900d62c91b5325537a3dea385a339b27112fb
krande <kristoffer_andersen@outlook.com>
2024-09-02 09:59:49 +0200

    fix lint

--- tests/profiling/test_large_ifc_models.py
+++ tests/profiling/test_large_ifc_models.py
@@ -1,5 +1,6 @@
-import ada
 import pytest
+
+import ada
 
 
 @pytest.mark.benchmark
@@ -7,7 +8,7 @@ def test_build_big_ifc():
     # create 10_000 beams and write it to ifc
     beams = []
     for i in range(0, 10_000):
-        bm = ada.Beam(f'bm{i}', (i, 0, 0), (i + 1, 0, 0), 'IPE300')
+        bm = ada.Beam(f"bm{i}", (i, 0, 0), (i + 1, 0, 0), "IPE300")
         beams.append(bm)
     a = ada.Assembly() / beams
     a.to_ifc(file_obj_only=True)
