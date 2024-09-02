0625c42c0d10af9b96317c84a16ddbc47146f52d
krande <kristoffer_andersen@outlook.com>
2024-09-02 09:53:38 +0200

    add ci profiling

---
+++ tests/profiling/test_large_ifc_models.py
@@ -0,0 +1,13 @@
+import ada
+import pytest
+
+
+@pytest.mark.benchmark
+def test_build_big_ifc():
+    # create 10_000 beams and write it to ifc
+    beams = []
+    for i in range(0, 10_000):
+        bm = ada.Beam(f'bm{i}', (i, 0, 0), (i + 1, 0, 0), 'IPE300')
+        beams.append(bm)
+    a = ada.Assembly() / beams
+    a.to_ifc(file_obj_only=True)
