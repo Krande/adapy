e0d9d3cc72957e9e64cc16412558e7a9cafd4414
krande <kristoffer_andersen@outlook.com>
2024-12-05 12:23:47 +0100

    include relative placement from product level on imported native ifc geometries

---
+++ tests/core/cadit/ifc/roundtripping/test_roundtrip_box.py
@@ -0,0 +1,16 @@
+import ada
+from ada.config import Config
+
+
+def test_basic_box():
+    box = ada.PrimBox('box1', (1, 2, 3), (4, 5, 6))
+    a = ada.Assembly() / box
+    a.ifc_store.sync()
+    Config().update_config_globally("ifc_import_shape_geom", True)
+    b = ada.from_ifc(a.ifc_store.f)
+    results = list(b.get_all_physical_objects())
+    assert len(results) == 1
+    rshape = results[0]
+    assert rshape.name == 'box1'
+    rbox = ada.PrimBox.from_box_geom(rshape.name, rshape.geom.geometry, metadata=rshape.metadata)
+    assert rbox.p1.is_equal(box.p1)
