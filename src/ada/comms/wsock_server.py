d25e3b18aae8fb3b41e0b95bd017296188e42571
krande <kristoffer_andersen@outlook.com>
2025-09-28 15:10:59 +0200

    chore: add target_id support to config, update websocket handling, and introduce trimesh visualization

--- src/ada/comms/wsock_server.py
+++ src/ada/comms/wsock_server.py
@@ -15,7 +15,7 @@ from ada.comms.fb_wrap_model_gen import CommandTypeDC, MessageDC, TargetTypeDC
 from ada.comms.msg_handling.default_on_message import default_on_message
 from ada.comms.scene_model import SceneBackend
 from ada.comms.wsockets_utils import client_from_str
-from ada.config import logger
+from ada.config import logger, Config
 from ada.procedural_modelling.procedure_store import ProcedureStore
 
 
@@ -159,10 +159,12 @@ class WebSocketAsyncServer:
             logger.debug(f"Forwarding message to {sender}")
         message_sent = False
         clients_to_remove = []
-        client_ids = [client.instance_id for client in self.connected_clients]
-        # if target_id is not None and target_id not in client_ids:
-        #     from ada.visit.rendering.renderer_react import RendererReact
-        #     RendererReact().show(target_id=target_id)
+        if Config().general_target_id_support:
+            client_ids = [client.instance_id for client in self.connected_clients]
+            if target_id is not None and target_id not in client_ids:
+                from ada.visit.rendering.renderer_react import RendererReact
+
+                RendererReact().show(target_id=target_id)
         for client in self.connected_clients:
             if client == sender:
                 continue
