38026d3449db77222fe948769889e9f5595ca32b
krande <kristoffer_andersen@outlook.com>
2026-01-26 10:39:03 +0100

    chore: clean up beams_offset.xml and update beam offsets test

--- tests/core/api/cog/test_cog_beam_offsets.py
+++ tests/core/api/cog/test_cog_beam_offsets.py
@@ -1,15 +1,16 @@
 import ada
 from ada import Assembly
 
-#Assembly Mass : 1916.916,  CoG  : (0.500, 2.440, 0.185)
 
-# todo seems like from_genie_xml does not import offsets correctly
-a: Assembly = ada.from_genie_xml(r"./files/beams_offset.xml")
-cog = a.calculate_cog()
-a.show()
-print(cog)
+def test_beam_offset_from_gxml(example_files):
+    # Assembly Mass : 1916.916,  CoG  : (0.500, 2.440, 0.185)
+    # todo seems like from_genie_xml does not import offsets correctly
+    a: Assembly = ada.from_genie_xml(example_files / "./fem_files/sesam/beams_offset.xml")
+    cog = a.calculate_cog()
+    a.show()
+    print(cog)
 
-assert round(cog.tot_mass,3) == 1916.916
-assert round(cog.p.x,3) == 0.500
-assert round(cog.p.y,3) == 2.440
-assert round(cog.p.z,3) == 0.185
+    assert round(cog.tot_mass, 3) == 1916.916
+    assert round(cog.p.x, 3) == 0.500
+    assert round(cog.p.y, 3) == 2.440
+    assert round(cog.p.z, 3) == 0.185
